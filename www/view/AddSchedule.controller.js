410a6ac3fac682fe47b0d93f8e1bbf5dad791c7b
David April <dsa157@gmail.com>
2014-08-01 16:28:19 -0500

    added support for local storage. changed list to use ObjectListItem so attributes can be shown

--- www/view/AddSchedule.controller.js
+++ www/view/AddSchedule.controller.js
@@ -20,25 +20,64 @@ sap.ui.controller("com.dsa157.ruok.view.AddSchedule", {
 		app.navBack();
 	},
 
+	getMessage: function() {
+		var msg = this.byId('msg').getValue();
+		if (msg == "") {
+			msg = "ROUK Reminder!";
+		}
+		return msg;
+	},
+
+	getDate: function() {
+		var timeStr = this.byId('dt').getValue();
+		//alert(timeStr);
+		var dt = new Date();
+
+		if (timeStr != "") {
+			var dateStr = (dt.getMonth()+1) + "/" + dt.getDate() + "/" + dt.getFullYear() + " ";	
+			//alert(dateStr);
+			dt = new Date(dateStr + timeStr);
+		}
+		return dt;
+	},
+	
+	getTimeStr: function() {
+		var timeStr = this.byId('dt').getValue();
+		if (timeStr == "") {
+			var dt = new Date();
+			var hrs = dt.getHours();
+			var min = dt.getMinutes();
+			min = (min < 10) ? "0" + min : min;
+			var ampm = (hrs > 12) ? "PM" : "AM";
+			hrs = (hrs > 12) ? hrs-12 : hrs;
+			timeStr = hrs + ":" + min + " " + ampm;	
+		}
+		return timeStr;
+	},
+
 	onPressSave: function(evt) {
-      var msg = this.byId('msg').getValue();
-      if (msg == "") {
-        msg = "ROUK Reminder!";
-      }
-      var dt = this.byId('dt').getValue();
-      if (dt == "") {
-        dt = new Date().getTime();
-      }
-                  
-      window.plugin.notification.local.add({
-        id: dt,
-        sound: './snd/clong.caf',
-        message: msg
-      });
+		var id = this.getTimeStr();
+		var msg = this.getMessage();
+		var dt = this.getDate();
+		//alert(dt);
+		var obj = {
+				"id": id,
+				"repeat": "daily",
+				"date": dt,
+				"sound": "./snd/clong.caf",
+				"message": msg
+		};
+		
+		localStorage.setItem(id, JSON.stringify(obj));
 
-      //alert("msg=" + msg);
-      //alert("dt=" + JSON.stringify(dt));
+		window.plugin.notification.local.add({
+			"id": obj.id,
+			"repeat": obj.repeat,
+			"date": obj.date,
+			"sound": obj.sound,
+			"message": obj.message
+		});
+		app.navBack();
 	}
-
 
 });
--- www/view/Schedule.controller.js
+++ www/view/Schedule.controller.js
@@ -5,41 +5,110 @@ sap.ui.controller("com.dsa157.ruok.view.Schedule", {
 	 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 	 * @memberOf com.dsa157.ruok.Schedule
 	 */
-//	onInit: function() {
+	onInit: function() {
 
-//	},
+	},
 
 	/**
 	 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 	 * (NOT before the first rendering! onInit() is used for that one!).
 	 * @memberOf com.dsa157.ruok.Schedule
 	 */
-//	onBeforeRendering: function() {
-
-//	},
+	onBeforeRendering: function() {
+		var list = new sap.m.List({ id: "list1" });
+		var vbox = this.byId("vbox");
+		vbox.addItem(list);
+		this.populateList();
+	},
 
 	/**
 	 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 	 * This hook is the same one that SAPUI5 controls get after being rendered.
 	 * @memberOf com.dsa157.ruok.Schedule
 	 */
-//	onAfterRendering: function() {
+	//	onAfterRendering: function() {
 
-//	},
+	//	},
 
 	/**
 	 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 	 * @memberOf com.dsa157.ruok.Schedule
 	 */
-//	onExit: function() {
+	//	onExit: function() {
 
-//	}
+	//	}
+
+	populateList: function() {
+		window.plugin.notification.local.getScheduledIds(
+				function (scheduledIds) {
+					var list = sap.ui.getCore().byId("list1");
+					list.destroyItems();
+					for (var i=0; i<scheduledIds.length; i++) {
+						var str = scheduledIds[i];
+						var li = new sap.m.ObjectListItem();
+						//alert("str=" + str);
+						var obj = JSON.parse(localStorage.getItem(str));
+						li.setTitle(obj.id);
+						var attr = new sap.m.ObjectAttribute();
+						attr.setText(obj.message);
+						li.addAttribute(attr);
+						//alert("n1=" + notifObject.id);
+						list.addItem(li);
+					}
+				}
+		);
+	},
 
 	onPressBack: function(evt) {
 		app.navBack();
 	},
 
+	onPressTest: function(evt) {
+		this.populateList();
+	},
+
+	onPressClear: function(evt) {
+		window.plugin.notification.local.getScheduledIds(
+				function (scheduledIds) {
+					for (var i=0; i<scheduledIds.length; i++) {
+						var str = scheduledIds[i];
+						localStorage.removeItem(str);
+					}
+				}
+		);
+
+		window.plugin.notification.local.cancelAll("handleCancelAll", "");
+		this.populateList();
+	},
+
+	onPressAutoAdd: function(evt) {
+		var dt = new Date();
+		var hrs = dt.getHours();
+		var min = dt.getMinutes();
+		var ampm = (hrs > 12) ? "PM" : "AM";
+		hrs = (hrs > 12) ? hrs-12 : hrs;
+		min = (min < 10) ? "0" + min : min;
+		var id = hrs + ":" + min + " " + ampm;	
+		var msg = "test 123";
+		var notifObject = {
+				"id": id,
+				"message": msg
+		};
+		//alert("n=" + notifObject.message);
+		alert("n2=" + JSON.stringify(notifObject));
+		localStorage.setItem(id, JSON.stringify(notifObject));
+		var x = JSON.parse(localStorage.getItem(id));
+		//alert("x=" + x);
+		alert("x2=" + JSON.stringify(x));
+
+		window.plugin.notification.local.add({
+			"id": id,
+			"message": msg
+		});
+		this.populateList();
+	},
+
 	onPressAdd: function(evt) {
-    		app.navTo("AddSchedule", evt);
-  	},
+		app.navTo("AddSchedule", evt);
+	},
 });
